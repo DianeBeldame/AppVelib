9df6588b9174dc38217e70331c5f1446687f1e21
TDST <thibaut.dusanter@gmail.com>
2016-10-28 18:42:21 +0200

    Mise a jour du script de lecture, travail sur les dates et debut de plot

--- Scripts/Read.R
+++ Scripts/Read.R
@@ -1,9 +1,10 @@
 library(jsonlite)
 library(dplyr)
 library(RJSONIO)
+library(ggplot2)
 
 
-inputFile <- "data_all_Paris.jjson_2016-09-01-1472703936"
+inputFile <- "data/data_all_Paris.jjson_2016-09-01-1472703936"
 con  <- file(inputFile, open = "r")
 data <- list()
 i <- 0
@@ -14,9 +15,26 @@ while (length(oneLine <- readLines(con, n = 1, warn = FALSE)) > 0) {
   i <- i +1
   if (i %% 100 == 0)
   {
-    i
+    printf(i)
   }
 } 
 
 close(con)
 
+# Parse date fields from posix
+# TODO: which timezone should we use?
+data$download_date_parsed <- as.POSIXct(data$download_date[1], origin="1970-01-01", tz="GMT")
+data$last_update_parsed <- as.POSIXct(data$last_update[1]/1000, origin="1970-01-01", tz="GMT")
+#retrieve day
+data$day <- weekdays(data$last_update_parsed, abbreviate = TRUE)
+#retrieve hour
+data$day <- weekdays(data$last_update_parsed, abbreviate = TRUE)
+# retrieve hour
+data$hour <- as.numeric(format(data$last_update_parsed, "%H"))
+
+# Plot the evolution of available_bikes
+data_901 <- filter(data, number == 901)
+plot(x = data_901$last_update, y = data_901$available_bikes, type = "l")
+
+# Plot 
+ggplot(data_901)+geom_point(aes(x=hour, y=available_bikes, col=day))
