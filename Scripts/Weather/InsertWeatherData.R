1c26359d930b834469517a9510e98b46bf2bb865
TDST <thibaut.dusanter@gmail.com>
2016-11-24 19:24:20 +0100

    Meteo: modification du script pour inserrer avec ou sans mot de passe

--- Scripts/Weather/InsertWeatherData.R
+++ Scripts/Weather/InsertWeatherData.R
@@ -10,8 +10,16 @@ lon <- 2.333333
 api_key           <- my.env$api_key_darksky
 #retrieve weather for the current datetime
 current_weather  <- jsonlite::fromJSON(sprintf("https://api.darksky.net/forecast/%s/%f,%f", api_key, lat, lon))
+current_weather_local_time <- as.POSIXct(current_weather$currently$time, origin="1970-01-01", tz=current_weather$timezone)
 
-m <- mongo(collection = "weather", db = "velibs")
+dbName <- "velib"
+if (!is.null(my.env$user_mongo) && !is.null(my.env$pwd_mongo)) {
+  mongo_url <- paste0("mongodb://",my.env$user_mongo,":",my.env$pwd_mongo,"@localhost/", dbName)
+} else {
+  mongo_url <- paste0("mongodb://localhost/", dbName)
+}
+print(paste0("Inserting weather data at ", current_weather_local_time, "(local time) in ", mongo_url))
+m <- mongo(collection = "weather", db = dbName, url = mongo_url)
 m$insert(current_weather)
 
 q('yes')
