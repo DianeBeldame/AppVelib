3afead18cc5a46073f5f36001820a9974d48d102
denismolin <denis_molin@yahoo.fr>
2016-12-19 17:16:20 +0100

    ajout des script de Heng Li

--- FonctionsUtiles.R
+++ FonctionsUtiles.R
@@ -11,17 +11,27 @@ My.Single.Query<-function(m,max_distance=200,address=NULL,day="lundi",nb_station
                              { "$skip"  : ',skip,'},
                              { "$limit" : ',limit_size,'},
                              { "$unwind": "$serie"},
+<<<<<<< HEAD
                              { "$match" : {"serie.day" : {"$in" :["',paste(day,collapse = '","'),'"]} ,
                              "serie.hour" : {"$in" : [',paste(hour,collapse = ","),']},
+=======
+                             { "$match" : {"serie.day" : "',day,'" ,
+                             "serie.hour" : ',hour,',
+>>>>>>> 442ed247a39d9c03582fb2738f186f8be41466f5
                              "serie.last_update" : {"$gte": ',as.integer(as.POSIXct(date_start, origin="1970-01-01", tz="GMT"))*1000,'},
                              "serie.last_update" : {"$lte": ',as.integer(as.POSIXct(date_end, origin="1970-01-01", tz="GMT"))*1000,'}}},
                              { "$project" : {"_id": 0, "number": 1, 
                              "serie.hour" : 1,
+<<<<<<< HEAD
                              "serie.status":1,
                              "serie.bike_stands":1,
                              "serie.available_bikes":1,
                              "serie.available_bike_stands":1,
                              "serie.day":1,
+=======
+                             "serie.available_bikes":1,
+                             "serie.available_bike_stands":1,
+>>>>>>> 442ed247a39d9c03582fb2738f186f8be41466f5
                              "serie.day_num":1,
                              "serie.month_num":1,
                              "serie.date":1,
@@ -29,9 +39,12 @@ My.Single.Query<-function(m,max_distance=200,address=NULL,day="lundi",nb_station
                              "serie.precipIntensity":1,
                              "serie.temperature":1,
                              "serie.humidity":1,
+<<<<<<< HEAD
                              "serie.minute":1,
                              "dist.calculated":1,
                              "dist.location":1,
+=======
+>>>>>>> 442ed247a39d9c03582fb2738f186f8be41466f5
                              "serie.windspeed":1,
                              "serie.visibility":1,
                              "serie.cloudcover":1}} 
@@ -61,6 +74,7 @@ My.Single.Query<-function(m,max_distance=200,address=NULL,day="lundi",nb_station
                              }
                              },
                              { "$unwind": "$serie"},
+<<<<<<< HEAD
                              { "$match" : {"serie.day" : {"$in" :["',paste(day,collapse = '","'),'"]} ,
                              "serie.hour" : {"$in" : [',paste(hour,collapse = ","),']},
                              "serie.last_update" : {"$gte": ',as.integer(as.POSIXct(date_start, origin="1970-01-01", tz="GMT"))*1000,'},
@@ -73,14 +87,32 @@ My.Single.Query<-function(m,max_distance=200,address=NULL,day="lundi",nb_station
                              "serie.available_bikes":1,
                              "serie.available_bike_stands":1,
                              "serie.day":1,
+=======
+                             { "$match" : {"serie.day" : "',day,'" ,
+                             "serie.hour" : ',hour,',
+                             "serie.last_update" : {"$gte": ',as.integer(as.POSIXct(date_start, origin="1970-01-01", tz="GMT"))*1000,'},
+                             "serie.last_update" : {"$lte": ',as.integer(as.POSIXct(date_end, origin="1970-01-01", tz="GMT"))*1000,'}}},
+                             { "$project" : {"_id": 0, "number": 1, 
+                             "serie.hour" : 1,
+                             "serie.available_bikes":1,
+                             "serie.available_bike_stands":1,
+>>>>>>> 442ed247a39d9c03582fb2738f186f8be41466f5
                              "serie.day_num":1,
                              "serie.month_num":1,
                              "serie.date":1,
                              "serie.summary":1,
+<<<<<<< HEAD
                              "serie.precipIntensity":1,"serie.precipProbability":1,"serie.apparentTemperature":1,
                              "serie.temperature":1,
                              "serie.humidity":1,
                              "serie.minute":1,
+=======
+                             "serie.precipIntensity":1,
+                             "serie.temperature":1,
+                             "serie.humidity":1,
+                             "serie.minute":1,
+                             "dist.calculated":1,
+>>>>>>> 442ed247a39d9c03582fb2738f186f8be41466f5
                              "dist.location":1,
                              "serie.windspeed":1,
                              "serie.visibility":1,
